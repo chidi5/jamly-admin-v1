1a12b46855d20fad58a038cf4fe4a0074f16c55d
chidi5 <joshdash.ji@gmail.com>
2024-04-03 00:49:02 +0100

    create store modal and form

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,19 @@
+"use client";
+
 import WidthWrapper from "@/components/WidthWrapper";
+import Modal from "@/components/ui/modal";
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { UserButton } from "@clerk/nextjs";
+import { useEffect } from "react";
 
 export default function Home() {
-  return (
-    <WidthWrapper>
-      <p>Hello World!</p>
-    </WidthWrapper>
-  );
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
+  return <WidthWrapper>Root Page</WidthWrapper>;
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { ClerkProvider } from "@clerk/nextjs";
+import ModalProvider from "@/providers/modal-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,7 +21,8 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={cn("relative h-full antialiased", inter.className)}>
-          <main className="relative flex flex-col min-h-screen">
+          <ModalProvider />
+          <main className="relative flex flex-col min-h-screen h-full">
             <div className="flex-grow flex-1">{children}</div>
           </main>
         </body>
