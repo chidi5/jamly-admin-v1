8cb11f831fa824fa29d6f65c55c63a5dc65b38c3
chidi5 <joshdash.ji@gmail.com>
2024-04-30 01:51:43 +0100

    welcome layout update

--- app/welcome/layout.tsx
+++ app/welcome/layout.tsx
@@ -1,21 +1,6 @@
-import { verifyAndAcceptInvitation } from "@/lib/queries";
-import { Plan } from "@prisma/client";
-import { redirect } from "next/navigation";
 import { ReactNode } from "react";
 
-const layout = async ({
-  children,
-  searchParams,
-}: {
-  children: ReactNode;
-  searchParams: { plan: Plan; state: string; code: string };
-}) => {
-  const storeId = await verifyAndAcceptInvitation();
-
-  if (storeId) {
-    redirect(`/store?plan=${searchParams.plan}`);
-  }
-
+const layout = async ({ children }: { children: ReactNode }) => {
   return <div>{children}</div>;
 };
 
--- app/welcome/page.tsx
+++ app/welcome/page.tsx
@@ -1,11 +1,24 @@
 "use client";
 
 import { useStoreModal } from "@/hooks/use-store-modal";
+import { verifyAndAcceptInvitation } from "@/lib/queries";
+import { Plan } from "@prisma/client";
+import { redirect } from "next/navigation";
 import { useEffect } from "react";
 
-export default function Home() {
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: { plan: Plan; state: string; code: string };
+}) {
   const onOpen = useStoreModal((state) => state.onOpen);
   const isOpen = useStoreModal((state) => state.isOpen);
+
+  const storeId = await verifyAndAcceptInvitation();
+
+  if (storeId) {
+    redirect(`/store?plan=${searchParams.plan}`);
+  }
 
   useEffect(() => {
     if (!isOpen) {
