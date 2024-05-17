3f644cc6569ada06413b7a7bf9e5d5233bdeaeda
chidi5 <joshdash.ji@gmail.com>
2024-05-17 16:21:08 +0100

    fix

--- app/welcome/layout.tsx
+++ app/welcome/layout.tsx
@@ -1,13 +1,9 @@
 import { verifyAndAcceptInvitation } from "@/lib/queries";
-import { currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 import { ReactNode } from "react";
 
 const layout = async ({ children }: { children: ReactNode }) => {
-  const user = await currentUser();
   const storeId = await verifyAndAcceptInvitation();
-
-  if (!user) redirect("/sign-in");
 
   if (storeId) {
     redirect("/store");
