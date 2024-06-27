d4d7ef611a4fc5838f73b6a77ba2d54fe6ed3002
chidi5 <joshdash.ji@gmail.com>
2024-06-27 14:56:44 +0100

    update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,7 @@
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    //domains: ["res.cloudinary.com"],
     remotePatterns: [
       {
         protocol: "https",
@@ -17,33 +12,6 @@ const nextConfig = {
     ],
   },
   reactStrictMode: false,
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    config.cache = {
-      type: "filesystem",
-      buildDependencies: {
-        config: [__filename],
-      },
-      cacheDirectory: path.resolve(__dirname, ".next/cache/webpack"),
-      name: `cache-${buildId}`,
-      store: "pack",
-      compression: "gzip",
-      profile: true,
-      customSerialize: (value) => {
-        if (typeof value === "string" && value.length > 1000) {
-          return Buffer.from(value);
-        }
-        return value;
-      },
-      customDeserialize: (value) => {
-        if (Buffer.isBuffer(value)) {
-          return value.toString();
-        }
-        return value;
-      },
-    };
-
-    return config;
-  },
 };
 
 export default nextConfig;
