5690c15d5ade84b2a0413b40ca14308400dd4248
chidi5 <joshdash.ji@gmail.com>
2024-06-27 22:19:27 +0100

    added suspense

--- app/(auth)/(routes)/join-team/page.tsx
+++ app/(auth)/(routes)/join-team/page.tsx
@@ -1,7 +1,12 @@
+import { Suspense } from "react";
 import { ExistingUserInviteForm } from "@/components/auth/existing-user-invite-form";
 
 const JoinTeamPage = () => {
-  return <ExistingUserInviteForm />;
+  return (
+    <Suspense>
+      <ExistingUserInviteForm />
+    </Suspense>
+  );
 };
 
 export default JoinTeamPage;
