7de91642b199faf1502d266772c0488eec0df571
chidi5 <joshdash.ji@gmail.com>
2024-06-27 21:27:53 +0100

    update

--- auth.ts
+++ auth.ts
@@ -1,12 +1,12 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
 import bcrypt from "bcryptjs";
 import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
-import { PrismaAdapter } from "@auth/prisma-adapter";
 import prismadb from "./lib/prismadb";
 
-import { SignInSchema } from "./schemas";
-import { getUserbyEmail, getUserbyId } from "./lib/queries/user";
 import { getTwoFactorConfirmationByUserId } from "./lib/queries/two-factor-confirmation";
+import { getUserbyEmail, getUserbyId } from "./lib/queries/user";
+import { SignInSchema } from "./schemas";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prismadb),
