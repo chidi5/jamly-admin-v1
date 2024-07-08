b1ac6bc4ca811507c5b329652b7d64e951fcac9f
chidi5 <joshdash.ji@gmail.com>
2024-07-09 00:27:31 +0100

    update

--- app/api/[storeId]/products/[productId]/route.ts
+++ app/api/[storeId]/products/[productId]/route.ts
@@ -121,23 +121,24 @@ export async function PATCH(
   try {
     const user = await currentUser();
 
-    if (!user)
+    if (!user) {
       return new NextResponse(
         JSON.stringify({ message: "Unauthenticated user!" }),
         { status: 403 }
       );
+    }
 
     const body = await request.json();
-
     await validateProductData(body);
 
-    if (!params.storeId)
+    if (!params.storeId) {
       throw new Error(JSON.stringify({ message: "Store id is required" }));
+    }
 
-    if (!params.productId)
+    if (!params.productId) {
       throw new Error(JSON.stringify({ message: "Product id is required" }));
+    }
 
-    // Fetch the store's default currency
     const store = await prismadb.store.findUnique({
       where: { id: params.storeId },
       select: { defaultCurrency: true },
@@ -173,211 +174,38 @@ export async function PATCH(
       );
     }
 
-    const updatedProduct = await prismadb.$transaction(
+    const transaction = await prismadb.$transaction(
       async (prisma) => {
-        const updatedProduct = await prisma.product.update({
-          where: { id: params.productId },
-          data: {
-            name: body.name,
-            handle:
-              body.name !== existingProduct.name
-                ? await generateUniqueProductHandle(body.name)
-                : existingProduct.handle,
-            description: body.description,
-            isFeatured: body.isFeatured,
-            isArchived: body.isArchived,
-            manageVariants: body.manageVariants,
-            weight: body.weight,
-            priceData: {
-              update: {
-                price: body.price,
-                discountedPrice: body.discountedPrice ?? null,
-                currency: store.defaultCurrency,
-              },
-            },
-            costAndProfitData: body.costProfit
-              ? {
-                  upsert: {
-                    create: {
-                      itemCost: body.costProfit.itemCost ?? 0,
-                      profit: body.costProfit.profit,
-                      profitMargin: body.costProfit.profitMargin,
-                      formattedItemCost:
-                        store.defaultCurrency +
-                          body.costProfit.itemCost?.toFixed(2) ||
-                        store.defaultCurrency + "0.00",
-                      formattedProfit:
-                        store.defaultCurrency +
-                          body.costProfit.profit?.toFixed(2) ||
-                        store.defaultCurrency + "0.00",
-                    },
-                    update: {
-                      itemCost: body.costProfit.itemCost ?? 0,
-                      profit: body.costProfit.profit,
-                      profitMargin: body.costProfit.profitMargin,
-                      formattedItemCost:
-                        store.defaultCurrency +
-                          body.costProfit.itemCost?.toFixed(2) ||
-                        store.defaultCurrency + "0.00",
-                      formattedProfit:
-                        store.defaultCurrency +
-                          body.costProfit.profit?.toFixed(2) ||
-                        store.defaultCurrency + "0.00",
-                    },
-                  },
-                }
-              : undefined,
-            stock: body.manageVariants
-              ? {
-                  delete: existingProduct.stock ? true : undefined,
-                }
-              : body.stock
-              ? {
-                  upsert: {
-                    create: {
-                      trackInventory: body.stock.trackInventory,
-                      quantity: body.stock.quantity ?? null,
-                      inventoryStatus: body.stock.inventoryStatus ?? "IN_STOCK",
-                    },
-                    update: {
-                      trackInventory: body.stock.trackInventory,
-                      quantity: body.stock.quantity ?? null,
-                      inventoryStatus: body.stock.inventoryStatus ?? "IN_STOCK",
-                    },
-                  },
-                }
-              : undefined,
-            discount:
-              body.discount && body.discount.value !== undefined
-                ? {
-                    upsert: {
-                      create: {
-                        value: body.discount.value,
-                        type: body.discount.type,
-                      },
-                      update: {
-                        value: body.discount.value,
-                        type: body.discount.type,
-                      },
-                    },
-                  }
-                : {},
-            images: {
-              deleteMany: {},
-              createMany: {
-                data: body.images.map((image: { url: string }) => ({
-                  url: image.url,
-                })),
-              },
-            },
-            additionalInfoSections: {
-              deleteMany: {},
-              create: body.additionalInfoSections?.map(
-                (info: { title: string; description: string }) => ({
-                  title: info.title,
-                  description: info.description,
-                })
-              ),
-            },
-            categories: {
-              set: body.categories?.map((id: string) => ({ id })),
-            },
-          },
-        });
-
-        if (body.options) {
-          await prisma.option.deleteMany({
-            where: { productId: params.productId },
-          });
-        }
-
-        const optionValues = await createOptionsAndValues(
+        const updatedProduct = await updateBasicProductInfo(
+          prisma,
           body,
-          updatedProduct.id
+          params.productId,
+          store.defaultCurrency,
+          existingProduct
         );
-
-        if (body.variants) {
-          await prisma.variant.deleteMany({
-            where: { productId: updatedProduct.id },
-          });
-
-          for (const variant of body.variants) {
-            const selectedOptionValues = variant.title.includes("|")
-              ? variant.title.split("|").map((value: string) => {
-                  const matchingOptionValue = optionValues.find(
-                    (data) => data!.value === value
-                  );
-                  if (!matchingOptionValue)
-                    throw new Error(
-                      JSON.stringify({
-                        message: `Option value "${value}" not found`,
-                      })
-                    );
-                  return { id: matchingOptionValue.id };
-                })
-              : [
-                  {
-                    id: optionValues.find(
-                      (data) => data!.value === variant.title
-                    )!.id,
-                  },
-                ];
-
-            await prisma.variant.create({
-              data: {
-                title: variant.title,
-                priceData: {
-                  create: {
-                    price: variant.price,
-                    discountedPrice: body.discountedPrice,
-                    currency: store.defaultCurrency,
-                  },
-                },
-                costAndProfitData: variant.costofgoods
-                  ? {
-                      create: {
-                        itemCost: variant.costofgoods,
-                        formattedItemCost:
-                          store.defaultCurrency +
-                            variant.costofgoods.toFixed(2) ||
-                          store.defaultCurrency + "0.00",
-                        profit:
-                          (body.discountedPrice!
-                            ? body.discountedPrice
-                            : variant.price) - variant.costofgoods,
-                        profitMargin:
-                          ((variant.price - variant.costofgoods) /
-                            (body.discountedPrice!
-                              ? body.discountedPrice
-                              : variant.price)) *
-                            100 ?? 0,
-                        formattedProfit:
-                          store.defaultCurrency +
-                          (
-                            (body.discountedPrice!
-                              ? body.discountedPrice
-                              : variant.price) - variant.costofgoods
-                          ).toFixed(2),
-                      },
-                    }
-                  : undefined,
-                stock:
-                  variant.inventory || variant.status
-                    ? {
-                        create: {
-                          trackInventory: variant.inventory ? true : false,
-                          quantity: variant.inventory ?? 0,
-                          inventoryStatus: variant.status ?? "IN_STOCK",
-                        },
-                      }
-                    : undefined,
-                product: { connect: { id: updatedProduct.id } },
-                selectedOptions: { connect: selectedOptionValues },
-              },
-            });
-          }
-        }
-
+        await updateProductImages(prisma, body.images, params.productId);
+        await updateAdditionalInfoSections(
+          prisma,
+          body.additionalInfoSections,
+          params.productId
+        );
+        await updateProductCategories(
+          prisma,
+          body.categories,
+          params.productId
+        );
+        const optionValues = await updateOptionsAndValues(
+          prisma,
+          body.options,
+          params.productId
+        );
+        await updateProductVariants(
+          prisma,
+          body.variants,
+          params.productId,
+          optionValues,
+          store.defaultCurrency
+        );
         return updatedProduct;
       },
       {
@@ -386,11 +214,301 @@ export async function PATCH(
       }
     );
 
-    return NextResponse.json(updatedProduct);
+    return NextResponse.json(transaction);
   } catch (error: any) {
     console.error("[PRODUCTS_PATCH]", error);
     return new NextResponse(JSON.stringify({ message: "Internal error" }), {
       status: 500,
     });
   }
+}
+
+// Functions for each part of the update process
+
+async function updateBasicProductInfo(
+  prisma: any,
+  body: any,
+  productId: string,
+  defaultCurrency: string,
+  existingProduct: any
+) {
+  const handle =
+    body.name !== existingProduct.name
+      ? await generateUniqueProductHandle(body.name)
+      : existingProduct.handle;
+
+  return prisma.product.update({
+    where: { id: productId },
+    data: {
+      name: body.name,
+      handle,
+      description: body.description,
+      isFeatured: body.isFeatured,
+      isArchived: body.isArchived,
+      manageVariants: body.manageVariants,
+      weight: body.weight,
+      priceData: {
+        update: {
+          price: body.price,
+          discountedPrice: body.discountedPrice ?? null,
+          currency: defaultCurrency,
+        },
+      },
+      costAndProfitData: body.costProfit
+        ? {
+            upsert: {
+              create: {
+                itemCost: body.costProfit.itemCost ?? 0,
+                profit: body.costProfit.profit,
+                profitMargin: body.costProfit.profitMargin,
+                formattedItemCost: formatCurrency(
+                  body.costProfit.itemCost,
+                  defaultCurrency
+                ),
+                formattedProfit: formatCurrency(
+                  body.costProfit.profit,
+                  defaultCurrency
+                ),
+              },
+              update: {
+                itemCost: body.costProfit.itemCost ?? 0,
+                profit: body.costProfit.profit,
+                profitMargin: body.costProfit.profitMargin,
+                formattedItemCost: formatCurrency(
+                  body.costProfit.itemCost,
+                  defaultCurrency
+                ),
+                formattedProfit: formatCurrency(
+                  body.costProfit.profit,
+                  defaultCurrency
+                ),
+              },
+            },
+          }
+        : undefined,
+      stock: body.manageVariants
+        ? existingProduct.stock
+          ? { delete: true }
+          : undefined
+        : body.stock
+        ? {
+            upsert: {
+              create: {
+                trackInventory: body.stock.trackInventory,
+                quantity: body.stock.quantity ?? null,
+                inventoryStatus: body.stock.inventoryStatus ?? "IN_STOCK",
+              },
+              update: {
+                trackInventory: body.stock.trackInventory,
+                quantity: body.stock.quantity ?? null,
+                inventoryStatus: body.stock.inventoryStatus ?? "IN_STOCK",
+              },
+            },
+          }
+        : undefined,
+      discount:
+        body.discount && body.discount.value !== undefined
+          ? {
+              upsert: {
+                create: {
+                  value: body.discount.value,
+                  type: body.discount.type,
+                },
+                update: {
+                  value: body.discount.value,
+                  type: body.discount.type,
+                },
+              },
+            }
+          : undefined,
+    },
+  });
+}
+
+async function updateProductImages(
+  prisma: any,
+  images: any[],
+  productId: string
+) {
+  await prisma.image.deleteMany({
+    where: { productId: productId },
+  });
+
+  if (images && images.length > 0) {
+    await prisma.image.createMany({
+      data: images.map((image: { url: string }) => ({
+        url: image.url,
+        productId: productId,
+      })),
+    });
+  }
+}
+
+async function updateAdditionalInfoSections(
+  prisma: any,
+  sections: any[],
+  productId: string
+) {
+  await prisma.additionalInfoSection.deleteMany({
+    where: { productId: productId },
+  });
+
+  if (sections && sections.length > 0) {
+    await prisma.additionalInfoSection.createMany({
+      data: sections.map((info: { title: string; description: string }) => ({
+        title: info.title,
+        description: info.description,
+        productId: productId,
+      })),
+    });
+  }
+}
+
+async function updateProductCategories(
+  prisma: any,
+  categories: any[],
+  productId: string
+) {
+  if (categories && categories.length > 0) {
+    await prisma.product.update({
+      where: { id: productId },
+      data: {
+        categories: {
+          set: categories.map((id: string) => ({ id })),
+        },
+      },
+    });
+  }
+}
+
+async function updateOptionsAndValues(
+  prisma: any,
+  options: any[],
+  productId: string
+) {
+  await prisma.option.deleteMany({
+    where: { productId: productId },
+  });
+
+  const optionValues = [];
+
+  if (options && options.length > 0) {
+    for (const option of options) {
+      const createdOption = await prisma.option.create({
+        data: {
+          name: option.name,
+          productId: productId,
+        },
+      });
+
+      for (const value of option.values) {
+        const createdValue = await prisma.optionValue.create({
+          data: {
+            value: value,
+            optionId: createdOption.id,
+          },
+        });
+        optionValues.push(createdValue);
+      }
+    }
+  }
+
+  return optionValues;
+}
+
+async function updateProductVariants(
+  prisma: any,
+  variants: any[],
+  productId: string,
+  optionValues: any[],
+  defaultCurrency: string
+) {
+  await prisma.variant.deleteMany({
+    where: { productId: productId },
+  });
+
+  if (variants && variants.length > 0) {
+    for (const variant of variants) {
+      const selectedOptionValues = getSelectedOptionValues(
+        variant.title,
+        optionValues
+      );
+
+      await prisma.variant.create({
+        data: {
+          title: variant.title,
+          priceData: {
+            create: {
+              price: variant.price,
+              discountedPrice: variant.discountedPrice,
+              currency: defaultCurrency,
+            },
+          },
+          costAndProfitData: variant.costofgoods
+            ? {
+                create: {
+                  itemCost: variant.costofgoods,
+                  formattedItemCost: formatCurrency(
+                    variant.costofgoods,
+                    defaultCurrency
+                  ),
+                  profit: calculateProfit(variant, defaultCurrency),
+                  profitMargin: calculateProfitMargin(variant),
+                  formattedProfit: formatCurrency(
+                    calculateProfit(variant, defaultCurrency),
+                    defaultCurrency
+                  ),
+                },
+              }
+            : undefined,
+          stock:
+            variant.inventory || variant.status
+              ? {
+                  create: {
+                    trackInventory: variant.inventory ? true : false,
+                    quantity: variant.inventory ?? 0,
+                    inventoryStatus: variant.status ?? "IN_STOCK",
+                  },
+                }
+              : undefined,
+          product: { connect: { id: productId } },
+          selectedOptions: { connect: selectedOptionValues },
+        },
+      });
+    }
+  }
+}
+
+// Utility functions
+
+function formatCurrency(value: number | undefined, currency: string): string {
+  return value !== undefined ? currency + value.toFixed(2) : currency + "0.00";
+}
+
+function calculateProfit(variant: any, defaultCurrency: string): number {
+  return (variant.discountedPrice ?? variant.price) - variant.costofgoods;
+}
+
+function calculateProfitMargin(variant: any): number {
+  return (
+    ((variant.price - variant.costofgoods) /
+      (variant.discountedPrice ?? variant.price)) *
+    100
+  );
+}
+
+function getSelectedOptionValues(title: string, optionValues: any[]): any[] {
+  return title.includes("|")
+    ? title.split("|").map((value: string) => {
+        const matchingOptionValue = optionValues.find(
+          (data) => data!.value === value
+        );
+        if (!matchingOptionValue) {
+          throw new Error(
+            JSON.stringify({ message: `Option value "${value}" not found` })
+          );
+        }
+        return { id: matchingOptionValue.id };
+      })
+    : [{ id: optionValues.find((data) => data!.value === title)!.id }];
 }
