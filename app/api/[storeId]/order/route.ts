bcc02d9511e031d805fc2e365e5da68c86d25615
chidi5 <joshdash.ji@gmail.com>
2024-07-16 01:05:25 +0100

    update order and orderItems

--- app/api/[storeId]/order/route.ts
+++ app/api/[storeId]/order/route.ts
@@ -1,5 +1,5 @@
-import { NextRequest, NextResponse } from "next/server";
 import prismadb from "@/lib/prismadb";
+import { NextRequest, NextResponse } from "next/server";
 
 function addressObjectToString(address: {
   address: string;
@@ -20,18 +20,9 @@ export async function POST(
     const body = await request.json();
     const { products, address, phone, customerId } = body;
 
-    console.log({ body });
-
     if (!products || products.length === 0) {
       return new NextResponse(
         JSON.stringify({ message: "Products are required" }),
-        { status: 400 }
-      );
-    }
-
-    if (!customerId) {
-      return new NextResponse(
-        JSON.stringify({ message: "Customer ID is required" }),
         { status: 400 }
       );
     }
@@ -65,6 +56,10 @@ export async function POST(
           in: products.map((product: { id: string }) => product.id),
         },
       },
+      include: {
+        variants: true,
+        stock: true,
+      },
     });
 
     if (productData.length !== products.length) {
@@ -74,26 +69,157 @@ export async function POST(
       );
     }
 
+    const orderItemsData = await Promise.all(
+      products.map(
+        async (product: {
+          id: string;
+          variant?: string;
+          variantQuantity: number;
+          variantPrice: number;
+        }) => {
+          const productInfo = productData.find((p) => p.id === product.id);
+
+          if (!productInfo) {
+            throw new Error(`Product with id ${product.id} not found`);
+          }
+
+          let stock;
+          if (
+            productInfo.variants &&
+            productInfo.variants.length > 0 &&
+            product.variant
+          ) {
+            const variant = productInfo.variants.find(
+              (v) => v.id === product.variant
+            );
+
+            if (!variant) {
+              throw new Error(
+                `Variant with id ${product.variant} not found for product ${product.id}`
+              );
+            }
+
+            stock = await prismadb.stock.findUnique({
+              where: { variantId: product.variant },
+            });
+
+            if (!stock) {
+              throw new Error(`Stock not found for variant ${variant.id}`);
+            }
+
+            if (
+              stock.trackInventory &&
+              stock.quantity! < product.variantQuantity
+            ) {
+              throw new Error(
+                `Insufficient stock for variant ${variant.id} of product ${product.id}`
+              );
+            }
+          } else {
+            stock = await prismadb.stock.findUnique({
+              where: { productId: product.id },
+            });
+
+            if (!stock) {
+              throw new Error(`Stock not found for product ${product.id}`);
+            }
+
+            if (
+              stock.trackInventory &&
+              stock.quantity! < product.variantQuantity
+            ) {
+              throw new Error(`Insufficient stock for product ${product.id}`);
+            }
+          }
+          return {
+            product: {
+              connect: { id: product.id },
+            },
+            variant: product.variant
+              ? {
+                  connect: { id: product.variant },
+                }
+              : undefined,
+            quantity: product.variantQuantity,
+            price: product.variantPrice,
+          };
+        }
+      )
+    );
+
     const order = await prismadb.order.create({
       data: {
         storeId: params.storeId,
         isPaid: true,
-        customerId,
+        customerId: customerId || null,
         phone,
         address: formattedAddress,
         status: "PROCESSING",
         orderItems: {
-          create: products.map(
-            (product: { id: string; variantPrice: number }) => ({
-              product: {
-                connect: { id: product.id },
-              },
-              price: product.variantPrice,
-            })
-          ),
+          create: orderItemsData,
         },
       },
     });
+
+    // Reduce stock after order is created
+    await Promise.all(
+      products.map(
+        async (product: {
+          id: string;
+          variant?: string;
+          variantQuantity: number;
+        }) => {
+          let stock;
+          if (product.variant) {
+            stock = await prismadb.stock.findUnique({
+              where: { variantId: product.variant },
+            });
+
+            if (stock?.trackInventory) {
+              await prismadb.stock.update({
+                where: { variantId: product.variant },
+                data: { quantity: { decrement: product.variantQuantity } },
+              });
+
+              if (stock.quantity! - product.variantQuantity <= 0) {
+                await prismadb.stock.update({
+                  where: { variantId: product.variant },
+                  data: { inventoryStatus: "OUT_OF_STOCK" },
+                });
+              }
+            } else {
+              await prismadb.stock.update({
+                where: { variantId: product.variant },
+                data: { inventoryStatus: "OUT_OF_STOCK" },
+              });
+            }
+          } else {
+            stock = await prismadb.stock.findUnique({
+              where: { productId: product.id },
+            });
+
+            if (stock?.trackInventory) {
+              await prismadb.stock.update({
+                where: { productId: product.id },
+                data: { quantity: { decrement: product.variantQuantity } },
+              });
+
+              if (stock.quantity! - product.variantQuantity <= 0) {
+                await prismadb.stock.update({
+                  where: { productId: product.id },
+                  data: { inventoryStatus: "OUT_OF_STOCK" },
+                });
+              }
+            } else {
+              await prismadb.stock.update({
+                where: { productId: product.id },
+                data: { inventoryStatus: "OUT_OF_STOCK" },
+              });
+            }
+          }
+        }
+      )
+    );
 
     return NextResponse.json({ success: true, order });
   } catch (error) {
