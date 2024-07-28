6dcccc2bb8de58e661c2cd31ec323118ef7f27e8
chidi5 <joshdash.ji@gmail.com>
2024-07-28 23:56:18 +0100

    updated layout

--- app/(auth)/(routes)/layout.tsx
+++ app/(auth)/(routes)/layout.tsx
@@ -4,7 +4,7 @@ export default function Layout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="flex items-center justify-center w-full min-h-screen bg-[url('/assets/blob-scene.svg')] bg-cover bg-no-repeat overflow-y-hidden">
+    <div className="flex items-center justify-center w-full min-h-screen bg-[url('/assets/blob-scene.svg')] bg-cover bg-no-repeat bg-fixed overflow-hidden box-border">
       {children}
     </div>
   );
